cccd4662bf6e652c6aae4cae1addf89e4ab85e30
Victor Hugo Tello <victorhugotello@hotmail.com>
2021-08-25 19:51:57 -0500

    initial commit

---
+++ src/main/java/com/victortello/ws/restassuredtest/RestAssuredTestApplication.java
@@ -0,0 +1,13 @@
+package com.victortello.ws.restassuredtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAssuredTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestAssuredTestApplication.class, args);
+	}
+
+}
